f7640d400ba1d78edffc5f5a2f4c7743f3be385f
Neonchen <neongen@web.de>
2011-10-13 21:23:15 +0300

    Remove: Unused "import java.util.ListIterator"

--- PermutationImpl.java
+++ PermutationImpl.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.List;
 import java.util.Set;

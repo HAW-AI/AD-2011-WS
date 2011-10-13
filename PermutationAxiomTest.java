9be7f44c1bb09f165620f16514562eb314f7f7dc
Neonchen <neongen@web.de>
2011-10-13 21:30:57 +0300

    Remove: Unused "import org.junit.AfterClass","import java.util.Set;","import java.util.HashSet;"

--- PermutationAxiomTest.java
+++ PermutationAxiomTest.java
@@ -1,8 +1,5 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
